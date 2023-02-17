ef04edf203a6e7e4dd27a842d3f48fa699d0cde4
Mathias Van Nuland <mathias.vannuland@gmail.com>
2023-02-17 21:56:41 +0100

    better way to accomplish the same screen behaviour

--- display.py
+++ display.py
@@ -4,13 +4,12 @@ class Display():
     def __init__(self, rw, rh):
         """
         Set the width and height of the window relative to the user screen.
-        Input:  - rw: ratioWidth, screenWidth / ratioWidth = displayWidth
-                - rh: ratioHeight, screenHeight / ratioHeight = displayHeight
+        Input:  - rw: (ratioWidth): screenWidth / ratioWidth = displayWidth
+                - rh: (ratioHeight): screenHeight / ratioHeight = displayHeight
         """
         # Get the width of the user display
-        display_width, display_height = pygame.display.Info().current_w, pygame.display.Info().current_h
-        self.w = float(display_width / rw) # Set the width of the pygame display relative the the user display
-        self.h = float(display_height / rh)
+        self.w = round(pygame.display.get_desktop_sizes()[0][0] / rw)
+        self.h = round(pygame.display.get_desktop_sizes()[0][1] / rh)
     
     def create_display(self):
         """Creates a display with a set width and height"""
