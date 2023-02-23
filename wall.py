16893d01fc8196b1fda5740fce5eed34a3b72a5f
jerome666888 <123131076+jerome666888@users.noreply.github.com>
2023-02-23 03:02:25 -0500

    bouncing for walls (buggy)

--- wall.py
+++ wall.py
@@ -10,19 +10,18 @@ class Wall(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, angle)
         self.rect = self.image.get_rect(topleft = position)
 
-        def collision(self, ball):
-            if pygame.sprite.collide_rect(self, ball):
-                # Get the center coordinates of the ball and the wall
-                ball_center = ball.rect.center
-                wall_center = self.rect.center
+    def collision(self, ball):
+        # Get the center coordinates of the ball and the wall
+        ball_center = ball.rect.center
+        wall_center = self.rect.center
 
-                # Calculate the angle between the centers of the ball and the wall
-                angle = math.atan2(ball_center[1] - wall_center[1], ball_center[0] - wall_center[0])
+        # Calculate the angle between the centers of the ball and the wall
+        angle = math.atan2(ball_center[1] - wall_center[1], ball_center[0] - wall_center[0])
 
-                # Calculate the new velocity of the ball
-                ball_speed = math.sqrt(ball.velocity[0] ** 2 + ball.velocity[1] ** 2)
-                new_speed = ball_speed * math.cos(angle)
-                new_velocity = [-new_speed * math.cos(angle), -new_speed * math.sin(angle)]
+        # Calculate the new velocity of the ball
+        ball_speed = math.sqrt(ball.velocity[0] ** 2 + ball.velocity[1] ** 2)
+        new_speed = ball_speed * math.cos(angle)
+        new_velocity = [-new_speed * math.cos(angle), -new_speed * math.sin(angle)]
 
-                # Update the ball's velocity
-                ball.velocity = new_velocity
+        # Update the ball's velocity
+        return new_velocity
