660808bdac5005e71124e3ba0dbc5422f0e556cd
jerome666888 <123131076+jerome666888@users.noreply.github.com>
2023-03-06 02:26:53 -0500

    Using SAT for collision detection

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 import pygame
+from math import sqrt, cos, sin, atan2, degrees
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, screen_width, screen_height, pos=[0, 0], startVelocity=[0, 0]):
@@ -12,15 +13,21 @@ class Ball(pygame.sprite.Sprite):
             base = screen_height
         ratio = 15
         self.image = pygame.transform.scale(self.image, (base/ratio, base/ratio))
+        # self.image = pygame.Surface([base/ratio, base/ratio])
+        # self.image.fill((255, 255, 255))
+        # self.image.set_colorkey((255, 255, 255))
+        # pygame.draw.circle(self.image, (0, 0, 0), (base/ratio/2, base/ratio/2), base/ratio/2)
 
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect()
+        self.rect.center = pos # TODO: also make the position dynamic
         self.velocity = startVelocity
         self.offScreen = False
+        self.radius = base/ratio/2
 
     def update(self, screen_width, screen_height):
         # TODO: create gravity variable
-        self.velocity = [self.velocity[0], self.velocity[1] + 0.1]
-        self.rect.move_ip(self.velocity)
+        self.velocity = [self.velocity[0], self.velocity[1]]
+        self.move()
 
         if self.rect.right < 0 or self.rect.left > screen_width:
             self.offScreen = True
@@ -28,3 +35,25 @@ class Ball(pygame.sprite.Sprite):
             self.offScreen = True
         else:
             self.offScreen = False
+
+
+    def move(self):
+        self.rect.move_ip(self.velocity)
+
+    def resolve_collision(self, collision_object):
+        
+        v = sqrt(pow(self.velocity[0], 2) + pow(self.velocity[1], 2))
+        if collision_object.__class__.__name__ == "Wall":
+            d = atan2(self.velocity[1], -self.velocity[0])
+            b = atan2(collision_object.rect.centerx - self.rect.centerx, collision_object.rect.centery - self.rect.centery)
+            a = d - b
+
+            print(degrees(a), degrees(b), degrees(d))
+
+            # Move out of the ball
+            self.rect.move_ip(-(self.velocity[0]), -(self.velocity[1]))
+            # Calculate new velocity
+            self.velocity[0] = -v * cos(a-b)
+            self.velocity[1] = -v * sin(a-b)
+
+            self.move()
