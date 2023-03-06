776c1aadedc23d03147fd1bd5f174c653c42b877
jerome666888 <123131076+jerome666888@users.noreply.github.com>
2023-03-06 02:28:37 -0500

    Using SAT for collision detection

--- wall.py
+++ wall.py
@@ -1,30 +1,58 @@
 import pygame
 import math
+from ball import Ball
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, width, height, angle = 0, position = [0,0]):
         super().__init__()
 
+        # Draw/setup the wall
         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
         self.image.fill((55, 55, 55))
-        self.image = pygame.transform.rotate(self.image, angle)
-        self.rect = self.image.get_rect(topleft = position)
+        self.rotated_image = pygame.transform.rotate(self.image, angle)
+        self.rect = self.rotated_image.get_rect(center = self.image.get_rect(center = position).center)
+        self.rect.center = position
+        self.image = self.rotated_image
+        self.rotated_vertices(width, height, angle, position)
 
-    def collision(self, ball):
-        # Get the center coordinates of the ball and the wall
-        ball_center = ball.rect.center
-        wall_center = self.rect.center
 
-        # Calculate the angle between the centers of the ball and the wall
-        angle = math.atan2(ball_center[1] - wall_center[1], ball_center[0] - wall_center[0])
+    def rotated_vertices(self, width, height, angle, position):
+        # Get the coordinates of the four vertices of the rectangle before rotation
+        x1, y1 = position[0] - width/2, position[1] - height/2
+        x2, y2 = position[0] + width/2, position[1] - height/2
+        x3, y3 = position[0] + width/2, position[1] + height/2
+        x4, y4 = position[0] - width/2, position[1] + height/2
 
-        # Calculate the new velocity of the ball
-        ball_speed = math.sqrt(ball.velocity[0] ** 2 + ball.velocity[1] ** 2)
-        new_speed = ball_speed * math.cos(angle)
-        new_velocity = [-new_speed * math.cos(angle), -new_speed * math.sin(angle)]
+        # Convert the angle from degrees to radians
+        angle = math.radians(angle)
+        # Rotate each vertices around the center of the the wall
+        cos_theta = math.cos(angle)
+        sin_theta = math.sin(angle)
+        cx, cy = position
+        x1, y1 = (x1 - cx) * cos_theta - (y1 - cy) * sin_theta + cx, (x1 - cx) * sin_theta + (y1 - cy) * cos_theta + cy
+        x2, y2 = (x2 - cx) * cos_theta - (y2 - cy) * sin_theta + cx, (x2 - cx) * sin_theta + (y2 - cy) * cos_theta + cy
+        x3, y3 = (x3 - cx) * cos_theta - (y3 - cy) * sin_theta + cx, (x3 - cx) * sin_theta + (y3 - cy) * cos_theta + cy
+        x4, y4 = (x4 - cx) * cos_theta - (y4 - cy) * sin_theta + cx, (x4 - cx) * sin_theta + (y4 - cy) * cos_theta + cy
 
-        # Update the ball's velocity
-        return new_velocity
+        # Update vertices of wall
+        self.rvertices = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+        print(self.rvertices)
 
-    #def collision_detection(self, ball_group):
-    
+    def collision_detection(self, balls):
+        for ball in balls:
+            p_axis = pygame.math.Vector2(self.rect.centerx - ball.rect.centerx, self.rect.centery - ball.rect.centery)
+            projection_min = None
+            projection_max = None
+            for vertice in self.rvertices:
+                vector = pygame.math.Vector2(vertice[0] - self.rect.centerx, vertice[1] - self.rect.centery)
+                projection = vector.dot(p_axis.normalize())
+                if projection_min is None or projection < projection_min:
+                    projection_min = projection
+                if projection_max is None or projection > projection_max:
+                    projection_max = projection
+            p_radius = ball.radius * p_axis.normalize()
+            p_length = p_axis.magnitude() - p_radius.magnitude()
+            if p_length > 0 or projection_max < -p_radius.magnitude() or projection_min > p_radius.magnitude():
+                continue
+            print("collision")
+            return True
